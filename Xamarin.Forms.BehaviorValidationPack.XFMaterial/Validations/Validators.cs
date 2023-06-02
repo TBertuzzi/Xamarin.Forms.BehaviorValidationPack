84e417ca82c49934c45bd9c86da722cc0c35445d
Guilherme <guirollzing@gmail.com>
2023-06-01 21:33:49 -0300

    Update Validators.cs
    - updated code to correctly handle the "System.FormatException" exception and perform basic CPF validation

--- Xamarin.Forms.BehaviorValidationPack.XFMaterial/Validations/Validators.cs
+++ Xamarin.Forms.BehaviorValidationPack.XFMaterial/Validations/Validators.cs
@@ -27,8 +27,6 @@ namespace Xamarin.Forms.BehaviorValidationPack.XFMaterial
             cpf = cpf.Trim();
             if (cpf.Length != 11)
                 return false;
-            if (cpf.Distinct().Count() == 1)
-                return false;
             
             int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
